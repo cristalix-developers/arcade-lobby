363d026c650af31cf56198e3d25197207177208f
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-01-23 12:44:06 +0300

    Update LootBoxManager.kt

--- node/src/main/kotlin/me/func/LootBoxManager.kt
+++ node/src/main/kotlin/me/func/LootBoxManager.kt
@@ -2,10 +2,12 @@ package me.func
 
 import dev.implario.bukkit.entity.StandHelper
 import dev.implario.bukkit.item.item
+import implario.humanize.Humanize
 import me.func.mod.Banners
 import me.func.protocol.element.Banner
 import net.minecraft.server.v1_12_R1.EnumItemSlot
 import net.minecraft.server.v1_12_R1.MinecraftServer
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftArmorStand
@@ -25,6 +27,18 @@ object LootBoxManager : Listener {
 
     private val lootbox = app.map.getLabels("lootbox").map { LootBox(it.clone().add(1.0, -0.6, 1.0)) }
 
+    init {
+        Bukkit.getScheduler().runTaskTimer(app, {
+            lootbox.forEach { lootbox ->
+                Bukkit.getOnlinePlayers().minByOrNull { it.location.distanceSquared(lootbox.origin) }?.let {
+                    lootbox.stand.headPose =
+                        lootbox.stand.headPose.setX(Math.toRadians(it.location.pitch.toDouble() - 15))
+                            .setY(Math.toRadians(it.location.yaw.toDouble() + 180))
+                }
+            }
+        }, 10, 1)
+    }
+
     @EventHandler
     fun TabCompleteEvent.handle() {
         isCancelled = true
@@ -40,7 +54,14 @@ object LootBoxManager : Listener {
         MinecraftServer.SERVER.postToNextTick {
             Arcade.get(player)?.let { data ->
                 lootbox.forEach {
-                    it.banner.content = "§bДоступно ${data.crates} лутбоксов"
+                    it.banner.content = "§bЛутбокс\n§fДоступно ${data.crates} ${
+                        Humanize.plurals(
+                            "штука",
+                            "штуки",
+                            "штук",
+                            data.crates
+                        )
+                    }\n"
                     Banners.show(player, it.banner)
                 }
             }
@@ -61,11 +82,13 @@ object LootBoxManager : Listener {
         val banner: Banner = Banner.Builder()
             .x(origin.x)
             .y(origin.y + 3.6)
-            .z(origin.z)
-            .watchingOnPlayer(true)
-            .weight(45)
-            .height(8)
-            .resizeLine(0, 0.4)
+            .z(origin.z + 2.0)
+            .yaw(-90f)
+            .pitch(10f)
+            .weight(65)
+            .height(14)
+            .resizeLine(0, 0.5)
+            .resizeLine(1, 0.5)
             .build()
     )
 }
