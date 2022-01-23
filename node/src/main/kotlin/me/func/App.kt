d9998ca83bf88b89a05620ca078cfdbbd46dfa39
fiwka <dropinclass1@gmail.com>
2022-01-23 13:21:11 +0500

    диванчик

--- node/src/main/kotlin/me/func/App.kt
+++ node/src/main/kotlin/me/func/App.kt
@@ -8,13 +8,20 @@ import dev.implario.games5e.sdk.cristalix.MapLoader
 import dev.implario.games5e.sdk.cristalix.WorldMeta
 import dev.implario.platform.impl.darkpaper.PlatformDarkPaper
 import me.func.misc.PersonalizationMenu
+import me.func.mod.Anime
+import me.func.mod.Npc
+import me.func.mod.Npc.onClick
+import me.func.protocol.dialog.*
+import me.func.protocol.npc.NpcBehaviour
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandExecutor
 import org.bukkit.entity.Player
+import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.plugin.java.JavaPlugin
 import ru.cristalix.core.CoreApi
 import ru.cristalix.core.inventory.IInventoryService
 import ru.cristalix.core.inventory.InventoryService
+import java.util.*
 
 lateinit var app: App
 
@@ -53,6 +60,160 @@ class App : JavaPlugin() {
                 PersonalizationMenu.open(sender)
             return@CommandExecutor true
         })
+
+        lobbyNpc(
+            Triple(-14.5, 88.0, -18.5),
+            "func",
+            UUID.fromString("307264a1-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-15.5, 88.0, -18.5),
+            "Faelan_",
+            UUID.fromString("6f3f4a2e-7f84-11e9-8374-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-16.5, 88.0, -18.5),
+            "rigb0s",
+            UUID.fromString("c155c00c-e4c0-11eb-acca-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-17.5, 88.0, -18.5),
+            "ZentoFX",
+            UUID.fromString("307b1c52-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-12.5, 88.0, -18.5),
+            "Fiwka1338",
+            UUID.fromString("845e92f3-7006-11ea-acca-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -18.5),
+            "_Demaster_",
+            UUID.fromString("303c31eb-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -17.5),
+            "DiamondDen",
+            UUID.fromString("ee476051-dc55-11e8-8374-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -16.5),
+            "Sworroo",
+            UUID.fromString("ae7abc6b-d142-11e8-8374-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -15.5),
+            "kasdo",
+            UUID.fromString("303dc644-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -14.5),
+            "Zabelov",
+            UUID.fromString("308380a9-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -14.5),
+            "WhiteNights",
+            UUID.fromString("3089411e-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-11.5, 88.0, -13.5),
+            "ItsPVX",
+            UUID.fromString("2bd88cc8-603c-11ec-acca-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-17.5, 88.0, -17.5),
+            "Sefeo",
+            UUID.fromString("30a1bff7-2c69-11e8-b5ea-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-17.5, 88.0, -16.5),
+            "Zenk__",
+            UUID.fromString("573f139e-57f5-11eb-acca-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-17.5, 88.0, -15.5),
+            "iLisov",
+            UUID.fromString("94964b0d-f545-11e8-8374-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-17.5, 88.0, -14.5),
+            "ONE1SIDE",
+            UUID.fromString("7f3fea26-be9f-11e9-80c4-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-17.5, 88.0, -13.5),
+            "BaggiYT",
+            UUID.fromString("64c67d57-a461-11e8-8374-1cb72caa35fd"),
+            null
+        )
+
+        lobbyNpc(
+            Triple(-14.5, 88.0, -15.5),
+            null,
+            UUID.fromString("ef2fb6fb-a6b5-11e8-8374-1cb72caa35fd"),
+            null,
+            sleeping = true,
+            sitting = false
+        )
+    }
+
+    fun lobbyNpc(blockPos: Triple<Double, Double, Double>, name: String?, uuid: UUID, dialog: Dialog?, sitting: Boolean = true, sleeping: Boolean = false) {
+        Npc.npc {
+            x = blockPos.first
+            y = blockPos.second
+            z = blockPos.third
+
+            this.sitting = sitting
+            this.sleeping = sleeping
+            behaviour = NpcBehaviour.STARE_AT_PLAYER
+
+            if (name != null)
+                this.name = name
+
+            skinUrl = "https://webdata.c7x.dev/textures/skin/$uuid"
+            skinDigest = uuid.toString()
+
+            onClick {
+                dialog ?: return@onClick
+
+                if (it.hand == EquipmentSlot.OFF_HAND)
+                    return@onClick
+                Anime.dialog(it.player, dialog, name ?: "")
+            }
+        }
     }
 
 }
